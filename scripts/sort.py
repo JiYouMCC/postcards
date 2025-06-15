75f5ba8605b0f8e946516ac82f6d4a4bdd1e0106
JiYouMCC <chengyi.ye@outlook.com>
2025-06-15 15:21:41 +0800

    Add cards

--- scripts/sort.py
+++ scripts/sort.py
@@ -1,12 +1,12 @@
-
 import csv
 from datetime import datetime
 
-file_path = "../_data/sent.csv"
+file_path = "../_data/received.csv"
 
 with open(file_path, mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     data = list(reader)
+
 
 def parse_date(date_string):
     date_formats = [
@@ -16,18 +16,18 @@ def parse_date(date_string):
         '%Y-%m-%d %H:%M',
         '%d-%b-%y'
     ]
-    
+
     for date_format in date_formats:
         try:
             return datetime.strptime(date_string, date_format)
         except ValueError:
             continue
-    
+
     return None
 
 
 for row in data:
-    row['parsed_date'] = parse_date(row['sent_date'])
+    row['parsed_date'] = parse_date(row['received_date'])
 
 sorted_data = sorted(
     [row for row in data if row['parsed_date'] is not None],
@@ -35,7 +35,7 @@ sorted_data = sorted(
 )
 
 with open(file_path, mode='w', encoding='utf-8', newline='') as file:
-    fieldnames = reader.fieldnames  
+    fieldnames = reader.fieldnames
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
     for row in sorted_data:
