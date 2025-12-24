b5b2b8a1e31c041d4103bdca4fa2576213cdda48
Jill Ye <jill.ye@perkinelmer.com>
2025-12-24 09:50:03 +0800

    降低map 精度

--- scripts/simple_world_geojson.py
+++ scripts/simple_world_geojson.py
@@ -9,6 +9,24 @@ for feature in data.get('features', []):
     properties = feature.get('properties', {})
     feature['properties'] = {'SOVEREIGNT': properties.get('SOVEREIGNT')}
 
+    # 简化几何数据，精度降低到小数点后4位
+    def simplify_coordinates(coords):
+        if isinstance(coords[0], list):
+            return [simplify_coordinates(c) for c in coords]
+        else:
+            return [round(c, 4) for c in coords]
+    geometry = feature.get('geometry', {})
+    geometry_type = geometry.get('type')
+    if geometry_type == 'Point':
+        geometry['coordinates'] = simplify_coordinates(geometry['coordinates'])
+    elif geometry_type in ['MultiPoint', 'LineString']:
+        geometry['coordinates'] = simplify_coordinates(geometry['coordinates'])
+    elif geometry_type in ['MultiLineString', 'Polygon']:
+        geometry['coordinates'] = [simplify_coordinates(part) for part in geometry['coordinates']]
+    elif geometry_type == 'MultiPolygon':
+        geometry['coordinates'] = [[simplify_coordinates(part) for part in polygon] for polygon in geometry['coordinates']]
+    feature['geometry'] = geometry
+
 # 将简化后的数据写入新文件
 with open('simplified_world.geojson', 'w', encoding='utf-8') as file:
     json.dump(data, file, separators=(',', ':'))
